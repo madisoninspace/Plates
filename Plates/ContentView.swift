dcaa43d3314d22e3544271adf5c4949127582b34
Madison L.H. Wass <931615+madisoninspace@users.noreply.github.com>
2024-06-11 20:56:41 -0400

    Initial Commit

---
+++ Plates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plates
+//
+//  Created by Madison L.H. Wass on 6/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plates/PlatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatesApp.swift
+//  Plates
+//
+//  Created by Madison L.H. Wass on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PlatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
